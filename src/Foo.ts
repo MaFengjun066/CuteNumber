0c5c36d71f452f9d676a4e02f1dceb8e0cd344c9
Fengjun Ma <FMa6@slb.com>
2022-10-25 10:59:01 +0800

    feat: add check biggest even number is greater than 10 and add unit test

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,27 @@ export class Foo {
     return 'bar'
   }
 }
+
+export function isCuteNumberIsGreeaterThan10(numbers:number[]):boolean
+{
+  const evenNumbers = findAllEvenNumbers(numbers)
+
+  const maxNumber = findMaxNumber(evenNumbers)
+
+  return isGreeaterThan10(maxNumber)
+}
+
+function findAllEvenNumbers(numbers:number[]):number[]
+{
+  return numbers.filter(number=>number%2===0)
+}
+
+function findMaxNumber(numbers:number[]):number
+{
+  return Math.max(...numbers)
+}
+
+function isGreeaterThan10(number:number):boolean
+{
+  return number>10
+}
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,41 @@
-import {Foo} from '../src/Foo'
+import {Foo,isCuteNumberIsGreeaterThan10} from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
+  })
+})
+
+describe('CuteNumber', ()=>{
+  it('should return true given number: 2,4,6,8,10,12,24',()=>{
+    const numbers = [2,4,6,8,10,12,24]
+
+    const actual = isCuteNumberIsGreeaterThan10(numbers)
+
+    expect(actual).toBeTruthy()
+  })
+
+  it('should return false given number: 2,4,6,8,10',()=>{
+    const numbers = [2,4,6,8,10]
+
+    const actual = isCuteNumberIsGreeaterThan10(numbers)
+
+    expect(actual).toBeFalsy()
+  })
+
+  it('should return false given number: 2,4,6,8',()=>{
+    const numbers = [2,4,6,8]
+
+    const actual = isCuteNumberIsGreeaterThan10(numbers)
+
+    expect(actual).toBeFalsy()
+  })
+
+  it('should return false given number: 1,3,5,7,9',()=>{
+    const numbers = [1,3,5,7,9]
+
+    const actual = isCuteNumberIsGreeaterThan10(numbers)
+
+    expect(actual).toBeFalsy()
   })
 })
